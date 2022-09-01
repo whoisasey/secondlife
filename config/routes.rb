afe96f1a59ec7498f92ccf957515358d05722fa5
whoisasey <atinachang@gmail.com>
2022-08-31 21:33:36 -0400

    add admins model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :admins
   resources :charities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "charities#index"
+    get 'signup', to: 'admins#new'
+  resources :admins,
 end
