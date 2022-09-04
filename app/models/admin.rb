0e9535206b5199719b20d84aa43ce57b7844f76c
whoisasey <atinachang@gmail.com>
2022-09-04 18:01:52 -0400

    add validations for admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,8 @@
 class Admin < ApplicationRecord
-  
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { maximum: 50 },
+            format: { with: VALID_EMAIL_REGEX }
 end
