7215933e4772530f68bfecf28bd16b642104c652
whoisasey <atinachang@gmail.com>
2022-09-04 17:47:16 -0400

    add admins table and model

--- db/migrate/20220904214326_create_admins.rb
+++ db/migrate/20220904214326_create_admins.rb
@@ -2,8 +2,6 @@ class CreateAdmins < ActiveRecord::Migration[7.0]
   def change
     create_table :admins do |t|
       t.string :email
-      t.string :password_digest
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_014154) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_04_214326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -39,13 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_014154) do
     t.string "summary"
     t.string "phone"
     t.integer "admin_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
