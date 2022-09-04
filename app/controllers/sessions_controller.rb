15c37fd860853b57214830162fe360d0ed2be772
whoisasey <atinachang@gmail.com>
2022-09-04 14:47:54 -0400

    remove admin model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new 
+    @admin = Admin.new
+  end
+
+  def create
+    admin = Admin.find_by(email: params[:session][:email].downcase)
+    binding.break
+    if admin && admin.authenticate(params[:session][:password])
+      session[:user_id] = admin.id
+      flash[:notice] = "Logged In Successfully"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+  def destroy
+    session[:admin_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "charities#index"
   get 'signup', to: 'admins#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
