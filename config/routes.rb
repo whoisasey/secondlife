5198b8dc522aa6af489886bf2b1677e0de213632
whoisasey <atinachang@gmail.com>
2022-09-05 15:42:16 -0400

    update admin signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :admins
   resources :charities
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   root "charities#index"
   get 'admin', to: 'admins#index'
-  get 'signup', to: 'admins#new'
+  get 'admin/signup', to: 'admins#new'
+  resources :admins, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
